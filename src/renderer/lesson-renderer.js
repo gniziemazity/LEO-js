0b33d0f0a5c669530024068756cf335fca476b3e
gniziemazity <gniziemazity@gmail.com>
2026-01-30 15:49:54 +0200

    Adding a pause between code blocks

--- src/renderer/lesson-renderer.js
+++ src/renderer/lesson-renderer.js
@@ -1,188 +1,194 @@
 const { ipcRenderer } = require("electron");
 
 class LessonRenderer {
-   constructor(lessonManager, uiManager, cursorManager) {
-      this.lessonManager = lessonManager;
-      this.uiManager = uiManager;
-      this.cursorManager = cursorManager;
-   }
+  constructor(lessonManager, uiManager, cursorManager) {
+    this.lessonManager = lessonManager;
+    this.uiManager = uiManager;
+    this.cursorManager = cursorManager;
+  }
 
-   render() {
-      const isTypingActive = this.uiManager.isActive();
+  render() {
+    const isTypingActive = this.uiManager.isActive();
 
-      this.uiManager.clearLessonContainer();
-      const executionSteps = [];
-      let globalStepCounter = 0;
+    this.uiManager.clearLessonContainer();
+    const executionSteps = [];
+    let globalStepCounter = 0;
 
-      const blocks = this.lessonManager.getAllBlocks();
+    const blocks = this.lessonManager.getAllBlocks();
 
-      blocks.forEach((block, blockIdx) => {
-         const blockDiv = this.uiManager.createBlockElement(block, blockIdx);
-         blockDiv.onclick = (e) => this.handleBlockClick(e, block, blockIdx);
+    blocks.forEach((block, blockIdx) => {
+      const blockDiv = this.uiManager.createBlockElement(block, blockIdx);
+      blockDiv.onclick = (e) => this.handleBlockClick(e, block, blockIdx);
 
-         if (block.type === "comment") {
-            globalStepCounter = this.renderCommentBlock(
-               blockDiv,
-               block,
-               blockIdx,
-               isTypingActive,
-               globalStepCounter,
-               executionSteps,
-            );
-         } else if (block.type === "code") {
-            globalStepCounter = this.renderCodeBlock(
-               blockDiv,
-               block,
-               blockIdx,
-               isTypingActive,
-               globalStepCounter,
-               executionSteps,
-            );
-         }
-
-         this.uiManager.appendToLessonContainer(blockDiv);
-      });
-
-      this.cursorManager.setExecutionSteps(executionSteps);
-
-      if (isTypingActive) {
-         this.cursorManager.updateCursor();
+      if (block.type === "comment") {
+        globalStepCounter = this.renderCommentBlock(
+          blockDiv,
+          block,
+          blockIdx,
+          isTypingActive,
+          globalStepCounter,
+          executionSteps,
+        );
+      } else if (block.type === "code") {
+        globalStepCounter = this.renderCodeBlock(
+          blockDiv,
+          block,
+          blockIdx,
+          isTypingActive,
+          globalStepCounter,
+          executionSteps,
+        );
       }
 
-      this.broadcastLessonData(executionSteps);
-   }
+      this.uiManager.appendToLessonContainer(blockDiv);
+    });
 
-   renderCommentBlock(
-      blockDiv,
-      block,
-      blockIdx,
-      isTypingActive,
-      globalStepCounter,
-      executionSteps,
-   ) {
-      blockDiv.contentEditable = !isTypingActive;
+    this.cursorManager.setExecutionSteps(executionSteps);
+
+    if (isTypingActive) {
+      this.cursorManager.updateCursor();
+    }
+
+    this.broadcastLessonData(executionSteps);
+  }
+
+  renderCommentBlock(
+    blockDiv,
+    block,
+    blockIdx,
+    isTypingActive,
+    globalStepCounter,
+    executionSteps,
+  ) {
+    blockDiv.contentEditable = !isTypingActive;
+    blockDiv.innerText = block.text;
+    blockDiv.oninput = () => {
+      this.lessonManager.updateBlock(blockIdx, blockDiv.innerText);
+    };
+
+    blockDiv.onpaste = (e) => {
+      e.preventDefault();
+      const text = e.clipboardData.getData("text/plain");
+      document.execCommand("insertText", false, text);
+    };
+
+    blockDiv.onkeydown = (e) => {
+      if (e.key === "Enter") {
+        e.preventDefault();
+        document.execCommand("insertText", false, "\n");
+      }
+    };
+
+    executionSteps.push({
+      type: "block",
+      element: blockDiv,
+      blockIndex: blockIdx,
+      globalIndex: globalStepCounter,
+    });
+
+    blockDiv.dataset.stepIndex = globalStepCounter;
+    return globalStepCounter + 1;
+  }
+
+  renderCodeBlock(
+    blockDiv,
+    block,
+    blockIdx,
+    isTypingActive,
+    globalStepCounter,
+    executionSteps,
+  ) {
+    const selectedBlockIndex = this.uiManager.getSelectedBlockIndex();
+
+    if (selectedBlockIndex === blockIdx && !isTypingActive) {
+      blockDiv.contentEditable = "true";
       blockDiv.innerText = block.text;
       blockDiv.oninput = () => {
-         this.lessonManager.updateBlock(blockIdx, blockDiv.innerText);
+        this.lessonManager.updateBlock(blockIdx, blockDiv.innerText);
       };
 
       blockDiv.onpaste = (e) => {
-         e.preventDefault();
-         const text = e.clipboardData.getData("text/plain");
-         document.execCommand("insertText", false, text);
+        e.preventDefault();
+        const text = e.clipboardData.getData("text/plain");
+        document.execCommand("insertText", false, text);
       };
 
       blockDiv.onkeydown = (e) => {
-         if (e.key === 'Enter') {
-            e.preventDefault();
-            document.execCommand('insertText', false, '\n');
-         }
+        if (e.key === "Enter") {
+          e.preventDefault();
+          document.execCommand("insertText", false, "\n");
+        }
       };
 
+      return globalStepCounter;
+    } else {
+      blockDiv.contentEditable = "false";
+      for (const char of block.text) {
+        const span = this.uiManager.createCharSpan(char, globalStepCounter);
+        blockDiv.appendChild(span);
+
+        executionSteps.push({
+          type: "char",
+          element: span,
+          char: char,
+          blockIndex: blockIdx,
+          globalIndex: globalStepCounter,
+        });
+        globalStepCounter++;
+      }
       executionSteps.push({
-         type: "block",
-         element: blockDiv,
-         blockIndex: blockIdx,
-         globalIndex: globalStepCounter,
+        type: "block",
+        element: blockDiv,
+        blockIndex: blockIdx,
+        globalIndex: globalStepCounter,
       });
 
       blockDiv.dataset.stepIndex = globalStepCounter;
       return globalStepCounter + 1;
-   }
+    }
+  }
 
-   renderCodeBlock(
-      blockDiv,
-      block,
-      blockIdx,
-      isTypingActive,
-      globalStepCounter,
-      executionSteps,
-   ) {
-      const selectedBlockIndex = this.uiManager.getSelectedBlockIndex();
+  handleBlockClick(e, block, blockIdx) {
+    const isTypingActive = this.uiManager.isActive();
 
-      if (selectedBlockIndex === blockIdx && !isTypingActive) {
-         blockDiv.contentEditable = "true";
-         blockDiv.innerText = block.text;
-         blockDiv.oninput = () => {
-            this.lessonManager.updateBlock(blockIdx, blockDiv.innerText);
-         };
+    if (!isTypingActive) {
+      if (this.uiManager.getSelectedBlockIndex() === blockIdx) return;
 
-         blockDiv.onpaste = (e) => {
-            e.preventDefault();
-            const text = e.clipboardData.getData("text/plain");
-            document.execCommand("insertText", false, text);
-         };
+      const clickX = e.clientX;
+      const clickY = e.clientY;
 
-         blockDiv.onkeydown = (e) => {
-            if (e.key === 'Enter') {
-               e.preventDefault();
-               document.execCommand('insertText', false, '\n');
-            }
-         };
-
-         return globalStepCounter;
+      this.uiManager.selectBlock(blockIdx);
+      this.render();
+      this.uiManager.focusBlock(blockIdx, clickX, clickY);
+    } else {
+      if (block.type === "code") {
+        const clickedSpan = e.target.closest(".char");
+        if (clickedSpan) {
+          this.cursorManager.jumpTo(parseInt(clickedSpan.dataset.stepIndex));
+        }
       } else {
-         blockDiv.contentEditable = "false";
-         for (const char of block.text) {
-            const span = this.uiManager.createCharSpan(char, globalStepCounter);
-            blockDiv.appendChild(span);
-
-            executionSteps.push({
-               type: "char",
-               element: span,
-               char: char,
-               blockIndex: blockIdx,
-               globalIndex: globalStepCounter,
-            });
-            globalStepCounter++;
-         }
-         return globalStepCounter;
+        const executionSteps = this.cursorManager.getExecutionSteps();
+        const step = executionSteps.find((s) => s.blockIndex === blockIdx);
+        if (step) {
+          this.cursorManager.jumpTo(step.globalIndex);
+        }
       }
-   }
+    }
+  }
 
-   handleBlockClick(e, block, blockIdx) {
-      const isTypingActive = this.uiManager.isActive();
+  broadcastLessonData(executionSteps) {
+    const blocks = this.lessonManager.getAllBlocks();
 
-      if (!isTypingActive) {
-         if (this.uiManager.getSelectedBlockIndex() === blockIdx) return;
-
-         const clickX = e.clientX;
-         const clickY = e.clientY;
-
-         this.uiManager.selectBlock(blockIdx);
-         this.render();
-         this.uiManager.focusBlock(blockIdx, clickX, clickY);
-      } else {
-         if (block.type === "code") {
-            const clickedSpan = e.target.closest(".char");
-            if (clickedSpan) {
-               this.cursorManager.jumpTo(
-                  parseInt(clickedSpan.dataset.stepIndex),
-               );
-            }
-         } else {
-            const executionSteps = this.cursorManager.getExecutionSteps();
-            const step = executionSteps.find((s) => s.blockIndex === blockIdx);
-            if (step) {
-               this.cursorManager.jumpTo(step.globalIndex);
-            }
-         }
-      }
-   }
-
-   broadcastLessonData(executionSteps) {
-      const blocks = this.lessonManager.getAllBlocks();
-
-      ipcRenderer.send("broadcast-lesson-data", {
-         blocks: blocks,
-         executionSteps: executionSteps.map((step) => ({
-            type: step.type,
-            blockIndex: step.blockIndex,
-            globalIndex: step.globalIndex,
-            char: step.char,
-         })),
-      });
-   }
+    ipcRenderer.send("broadcast-lesson-data", {
+      blocks: blocks,
+      executionSteps: executionSteps.map((step) => ({
+        type: step.type,
+        blockIndex: step.blockIndex,
+        globalIndex: step.globalIndex,
+        char: step.char,
+      })),
+    });
+  }
 }
 
 module.exports = LessonRenderer;
